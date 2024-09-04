8f944f1bd01623f70b2eb0e4fa7e6107b45b2934
RohanKVerma <rohan.verma@axeno.co>
2024-09-04 11:12:32 +0530

    Bug fixed

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-const accordion = document.querySelector('.accordionContainer');
-accordion.addEventListener('click', function(e){
-      for (let i = 0; i < accordion.childElementCount; i++) {
-            if (accordion.children[i].classList.contains('active')) {
-                  accordion.children[i].classList.toggle('active');
-            }
-      }
-})
-      
-accordion.addEventListener('click', function(e){
-      if (e.target.parentNode.classList.contains('accordionItem')) {
-            e.target.parentNode.classList.toggle('active');
-      }
-      if(e.target.parentNode.classList.contains('collapseBtn')){
-            e.target.parentNode.parentNode.classList.toggle('active');
-      }
-      if(e.target.parentNode.parentNode.classList.contains('heading')){
-            e.target.parentNode.parentNode.parentNode.classList.toggle('active');
-      }
+const accordionItems = document.querySelectorAll('.accordionItem');
+accordionItems.forEach(item => {
+      item.classList.remove('active');
+           
+      item.addEventListener('click', function(){
+            accordionItems.forEach((otherItem) =>{
+                  if(otherItem !== item){
+                        otherItem.classList.remove('active');
+                  }
+            })  
+            this.classList.toggle('active');
+      })
 })
